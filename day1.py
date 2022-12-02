0fb37b3696f6dfbefb531f89f5406c2ea4f0b2df
WojtekWnorowski <64868065+WojtekWnorowski@users.noreply.github.com>
2022-12-02 21:38:29 +0000

    day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,26 @@
+# def suma()
 
+
+with open("input1.txt") as plik:
+    dane = plik.readlines()
+    for i in range(len(dane)):
+        dane[i] = dane[i].rstrip()
+    print(dane)
+
+    # zadanie 1
+    skrzaty = []
+    k = 0
+    skrzat = 0
+    for i in range(len(dane)):
+        if dane[i] != '':
+            skrzat += int(dane[i])
+        else:
+            skrzaty.append(skrzat)
+            skrzat = 0
+    print(skrzaty)
+    # print(len(skrzaty))
+    # print(dane.count(''))
+    # print(max(skrzaty))
+    # zadanie 2
+    skrzaty.sort()
+    print(sum(skrzaty[-3:]))
